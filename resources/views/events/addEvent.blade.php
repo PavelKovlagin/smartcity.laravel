d1b61f6c35faac80ca342eada23eeaaeab9a6185
Pavel <pashokkov@mail.ru>
2020-06-10 16:47:57 +0300

    button center user position

--- resources/views/events/addEvent.blade.php
+++ resources/views/events/addEvent.blade.php
@@ -3,8 +3,15 @@
 Добавить событие
 @endsection
 @section('content')
-<script>
-    function MyPosition(){
+
+<script src="//api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+<script type="text/javascript">
+    ymaps.ready(init);
+    var myMap;
+    var longitude = 40.406635;
+    var latitude = 56.129057;
+
+    function changeMyCoords(){
 
         var options = {
             enableHighAccuracy: true,
@@ -14,15 +21,57 @@
 
         function success(pos) {
             var crd = pos.coords;
-            document.getElementById('longitude').value = crd.longitude ;
-            document.getElementById('latitude').value = crd.latitude;
+            longitude = crd.longitude;
+            latitude = crd.latitude;
+            document.getElementById('longitude').value = longitude;
+            document.getElementById('latitude').value = latitude;
+            myMap.destroy();
+            init(); 
         };
 
         function error(err) {
             alert("Ошибка геолокации. Код ошибки: " + err.code + ". Сообщение: " + err.message);
         };
 
-        navigator.geolocation.getCurrentPosition(success, error, {enableHighAccuracy: true, timeout: 5000, maximumAge: 0});
+        navigator.geolocation.getCurrentPosition(success, error, {enableHighAccuracy: true, timeout: 5000, maximumAge: 0});                                                               
+    }                            
+
+    function init() {
+        myMap = new ymaps.Map("map", {
+            center: [latitude, longitude], // Владимир
+            zoom: 11
+        }, {
+            balloonMaxWidth: 200,
+            searchControlProvider: 'yandex#search'
+        });
+
+        // Обработка события, возникающего при щелчке
+        // левой кнопкой мыши в любой точке карты.
+        
+        myMap.events.add('click', function (e) {
+            if (!myMap.balloon.isOpen()) {
+                myMap
+                var coords = e.get('coords');
+                document.getElementById('longitude').value =  coords[1].toPrecision(6);
+                document.getElementById('latitude').value = coords[0].toPrecision(6);
+                myMap.balloon.open(coords, {
+                    contentHeader:'Координаты события',
+                    contentBody:'<p>Координаты собития сохранены в текстовые поля.</p>' + [
+                        coords[0].toPrecision(6),
+                        coords[1].toPrecision(6)
+                        ].join(', ') + '</p>',
+                    contentFooter:'<sup>Щелкните еще раз</sup>'
+                });
+            }
+            else {
+                myMap.balloon.close();
+            }
+        });
+        
+        // Скрываем хинт при открытии балуна.
+        myMap.events.add('balloonopen', function (e) {
+            myMap.hint.close();
+        });
     }
 </script>
 
@@ -52,54 +101,12 @@
                         <p> Широта:</p>
                         <input class="form-control" required type="number" step="any" id="latitude" name="latitude">
                         <br>
-                        <input class="btn btn-outline-success my-2 my-sm-0" type="button" value="Мое местоположение" onClick="MyPosition()">
-
-                        <div class="row justify-content-center" style="text-align: center">
-                            <div id="map" style="width:640px; height:400px"></div>
-                        </div>
-                        <script src="//api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
-                        <script type="text/javascript">
-                            ymaps.ready(init);
-                            var myMap;
-
-                            function init () {
-                                myMap = new ymaps.Map("map", {
-                                    center: [56.129057, 40.406635], // Владимир
-                                    zoom: 11
-                                }, {
-                                    balloonMaxWidth: 200,
-                                    searchControlProvider: 'yandex#search'
-                                });
-
-                                // Обработка события, возникающего при щелчке
-                                // левой кнопкой мыши в любой точке карты.
-                                
-                                myMap.events.add('click', function (e) {
-                                    if (!myMap.balloon.isOpen()) {
-                                        var coords = e.get('coords');
-                                        document.getElementById('longitude').value =  coords[1].toPrecision(6);
-                                        document.getElementById('latitude').value = coords[0].toPrecision(6);
-                                        myMap.balloon.open(coords, {
-                                            contentHeader:'Координаты события',
-                                            contentBody:'<p>Координаты собития сохранены в текстовые поля.</p>' + [
-                                                coords[0].toPrecision(6),
-                                                coords[1].toPrecision(6)
-                                                ].join(', ') + '</p>',
-                                            contentFooter:'<sup>Щелкните еще раз</sup>'
-                                        });
-                                    }
-                                    else {
-                                        myMap.balloon.close();
-                                    }
-                                });
-                                
-                                // Скрываем хинт при открытии балуна.
-                                myMap.events.add('balloonopen', function (e) {
-                                    myMap.hint.close();
-                                });
-                            }
-                            </script>
-
+                        <input class="btn btn-outline-success my-2 my-sm-0" type="button" value="Мое местоположение" onClick="changeMyCoords()">
+                        <br>
+                        <div class="row justify-content-center" style="text-align: center">                            
+                            <div id="map" style="width:640px; height:400px"></div>                            
+                        </div> 
+                        <br>               
                         <p>Изображения</p>
                         <input class="btn btn-outline-success my-2 my-sm-0" multiple type="file" name="images[]" accept="image/*">
                         <br><br><br>
